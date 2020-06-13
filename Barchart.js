9268e709b3e5a530b54d962672b71293f432fb8f
flobeira <flobeira@sandiego.edu>
2020-06-13 13:47:23 -0700

    Plots using pandas

--- Barchart.js
+++ Barchart.js
@@ -4,35 +4,54 @@
 
 // Create the bar chart race vs arrest count
 
-function buildCharts(sample) {
-    d3.json("sf_police_data.json").then((data) => {
+//function buildCharts(race) {
+  //  d3.json("sf_police_data.json").then((data) => {
 
-        var samples = data.samples;
-        var resultArray = samples.filter(sampleObj => sampleObj.id == sample);
-        var result = resultArray[0];
+        function loadData(incident_race) {
+            d3.json("sf_police_data.json").then(function(data) {
+                var filteredData = data.filter(incident => incident.subject_race === incident_race)
+                console.log(filteredData);
+            })
+        }
+        loadData("hispanic");
 
-        var otu_ids = result.otu_ids;
-        var otu_labels = result.otu_labels;
-        var sample_values = result.sample_values;
-
-
-        var yticks = subject_race 
+        //var samples = data;
+        //console.log(data[0])
+        //var resultArray = data.filter(sampleObj => sampleObj.subject_race === race);
+        //var resultArray = data.filter(sampleObj => sampleObj.subject_race);
+        //console.log(resultArray)
+        //var result = resultArray[0];
         
-        var barData = [
-            {
-                y: yticks,
-                x: sample_values.slice(0, 10).reverse(),
-                text: otu_labels.slice(0, 10).reverse(),
-                type: "bar",
-                orientation: "h",
-            }
-            ];
+        //var race = result.subject_race;
+        //console.log(race)
+        //var arrests = result.arrest_made;
+        
+        // var yticks = race
+        
+    //    var barData = [
+      //      {
+        //        y: race,
+          //      x: arrests.slice(0, 10).reverse(),
+            //    text: otu_labels.slice(0, 10).reverse(),
+              //  type: "bar",
+                //orientation: "h",
+           // }
+            //];
 
-            var barLayout = {
-            title: "Race vs Arrest Count",
-            margin: { t: 30, l: 150 }
-            };
+            // var barLayout = {
+           //  title: "Race vs Arrest Count",
+           // margin: { t: 30, l: 150 }
+            // };
 
-            Plotly.newPlot("bar", barData, barLayout);
-        });
-}
+            // Plotly.newPlot("bar", barData, barLayout);
+       // });
+//}
+
+//function init(){
+//    var firstSample = sample[0];
+//    buildCharts(firstSample);
+//}
+
+//init();
+
+// buildCharts("hispanic");
